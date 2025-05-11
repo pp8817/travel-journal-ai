1938734927398f5725d5768c1a3cb00ff755715b
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-05-11 15:04:15 +0900

    Initial commit

---
+++ src/main/java/com/travel/travel_journal_ai/TravelJournalAiApplication.java
@@ -0,0 +1,13 @@
+package com.travel.travel_journal_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelJournalAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelJournalAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travel/travel_journal_ai/TravelJournalAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.travel_journal_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelJournalAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
