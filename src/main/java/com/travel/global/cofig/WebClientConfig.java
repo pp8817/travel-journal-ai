bbff96175bc816d57ba583a264926866ca988888
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-06-04 20:08:42 +0900

    fix: AI 서버 주소 변경

--- src/main/java/com/travel/global/cofig/WebClientConfig.java
+++ src/main/java/com/travel/global/cofig/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.travel.global.cofig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${ai.server.url}")
+    private String aiServerUrl;
+
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+    public WebClient aiWebClient() {
+        return WebClient.builder()
+                .baseUrl(aiServerUrl)
+                .build();
     }
 }
