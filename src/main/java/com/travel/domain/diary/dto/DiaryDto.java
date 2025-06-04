9387388b3692914403e19624ea6acf2b2fdccb17
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-06-04 20:08:26 +0900

    fix: 역직렬화를 위해 필드명 변경

--- src/main/java/com/travel/domain/diary/dto/DiaryDto.java
+++ src/main/java/com/travel/domain/diary/dto/DiaryDto.java
@@ -1,5 +1,6 @@
 package com.travel.domain.diary.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.travel.domain.diary.model.Visibility;
 import lombok.Builder;
 
@@ -17,24 +18,37 @@ public class DiaryDto {
             String weather,
             String companion,
             List<String> emotions,
-            Visibility visibility
+            Visibility visibility,
+            String image
     ) {
     }
 
     @Builder
     public record AiDiaryRequest(
+            @JsonProperty("date")
             String date,
+
+            @JsonProperty("location")
             String location,
+
+            @JsonProperty("emotions")
             List<String> emotions,
+
+            @JsonProperty("weather")
             String weather,
-            String companion
+
+            @JsonProperty("companion")
+            String companion,
+
+            @JsonProperty("image")
+            String image
     ) {}
 
     /**
      * Response DTO
      */
     public record AiDiaryResponse(
-            String content
+            String diary
     ) {}
 
     public record DiaryResponse(
