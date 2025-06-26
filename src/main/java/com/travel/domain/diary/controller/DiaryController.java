cace70c4f21e447590203af48a118acda6ad97f7
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-06-26 18:23:48 +0900

    fix: import 쉊ㅇ

--- src/main/java/com/travel/domain/diary/controller/DiaryController.java
+++ src/main/java/com/travel/domain/diary/controller/DiaryController.java
@@ -1,11 +1,15 @@
 package com.travel.domain.diary.controller;
 
-import com.travel.domain.diary.dto.DiaryDto;
+import com.travel.domain.diary.dto.request.CreateDiaryRequest;
+import com.travel.domain.diary.dto.response.DiaryResponse;
 import com.travel.domain.diary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -16,8 +20,8 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @PostMapping
-    public ResponseEntity<DiaryDto.DiaryResponse> createDiary(@RequestBody DiaryDto.CreateDiaryRequest request) {
-        DiaryDto.DiaryResponse response = diaryService.createDiary(request);
+    public ResponseEntity<DiaryResponse> createDiary(@RequestBody CreateDiaryRequest request) {
+        DiaryResponse response = diaryService.createDiary(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
