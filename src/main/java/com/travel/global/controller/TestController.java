6f4027dce8acbfeea4b33582272aebfae297882e
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-08-16 16:11:27 +0900

    fix: diary - images 순환관계 제거

--- src/main/java/com/travel/global/controller/TestController.java
+++ src/main/java/com/travel/global/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.travel.global.controller;
 
+import com.travel.domain.diary.dto.response.PinResponse;
+import com.travel.domain.image.dto.ImageMetaData;
 import com.travel.domain.image.dto.PlaceInfo;
 import com.travel.global.util.GooglePlacesUtil;
+import com.travel.global.util.ImageUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/test")
@@ -15,13 +22,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     private final GooglePlacesUtil googlePlacesUtil;
+    private final ImageUtil imageUtil;
 
     @GetMapping("/geocode")
     public ResponseEntity<?> testGeocoding(
-            @RequestParam("lat") double lat,
-            @RequestParam("lon") double lon
+            List<MultipartFile> images
     ) {
-        PlaceInfo placeInfo = googlePlacesUtil.getExactPlaceInfo(lat, lon);
-        return ResponseEntity.ok().body(placeInfo);
+        List<ImageMetaData> rawPins = imageUtil.extractMetadata(images);
+        List<PinResponse> response = rawPins.stream()
+                .sorted(Comparator.comparing(ImageMetaData::timestamp))
+                .map(meta -> {
+                    PlaceInfo placeInfo = googlePlacesUtil.getExactPlaceInfo(meta.latitude(), meta.longitude());
+
+                    return new PinResponse(
+                            meta.latitude(),
+                            meta.longitude(),
+                            meta.timestamp(),
+                            meta.fileName(),
+                            placeInfo.name(),
+                            placeInfo.vicinity()
+                    );
+                })
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(response);
     }
 }
